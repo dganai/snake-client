bb5e2a17419f7edab3ca9e6ab70aa00368780e01
dganai <dinaganai@gmail.com>
2022-01-19 20:02:56 +0000

    adding event listener

--- play.js
+++ play.js
@@ -1,6 +1,24 @@
 const { connect } = require('./client');
 
 
+// setup interface to handle user input from stdin
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = (data) => {
+  if (data === '\u0003') {
+    process.exit();
+  }
+}
+
 
 console.log('connecting ...');
 connect();
+setupInput();
